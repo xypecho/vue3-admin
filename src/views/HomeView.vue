7a1ae4bcc25466879532dd8ffadbac069a2cc01b
xypecho <xypecho@163.com>
2023-02-16 22:43:04 +0800

    初始化仓库

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <el-row class="mb-4">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </el-row>
+  </main>
+</template>
+<script setup lang="ts">
+import TheWelcome from "../components/TheWelcome.vue";
+</script>
